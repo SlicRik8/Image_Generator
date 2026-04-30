46d88085f89a8534b272156db71163da76a6eff5
Rakshit Dhoundiyal <rakshitdhoundiyal@gmail.com>
2026-04-30 17:47:32 +0530

    some change

--- src/main/java/com/ai/SpringAiDemo/GenAIController.java
+++ src/main/java/com/ai/SpringAiDemo/GenAIController.java
@@ -2,9 +2,13 @@ package com.ai.SpringAiDemo;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 @RequiredArgsConstructor
 @RestController
 public class GenAIController {
